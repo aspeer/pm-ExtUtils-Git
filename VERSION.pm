b47682b13ee4f1ac043c14442ac215f58a578f91
aspeer <aspeer>
2004-02-04 05:09:08 +0000

    Version increment

--- VERSION.pm
+++ VERSION.pm
@@ -20,6 +20,6 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 #
-#  $Id: VERSION.pm,v 1.33 2004/02/04 04:37:06 aspeer Exp $
+#  $Id: VERSION.pm,v 1.34 2004/02/04 05:09:08 aspeer Exp $
 
-$VERSION=qw($Revision: 1.33 $)[1];
+$VERSION=qw($Revision: 1.34 $)[1];

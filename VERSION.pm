52cf2579c299f9e58693634c032fe1f66f7196d6
aspeer <aspeer>
2004-02-05 11:31:39 +0000

    ExtUtils-CVS version increment

--- VERSION.pm
+++ VERSION.pm
@@ -20,6 +20,6 @@
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 #
-#  $Id: VERSION.pm,v 1.42 2004/02/05 06:10:40 aspeer Exp $
+#  $Id: VERSION.pm,v 1.43 2004/02/05 11:31:39 aspeer Exp $
 
-$VERSION=qw($Revision: 1.42 $)[1];
+$VERSION=qw($Revision: 1.43 $)[1];

cede56b7728c9629a17f99adff3f2135c20b1689
Andrew Speer <andrew.speer@isolutions.com.au>
2013-05-06 23:32:08 +0930

    Tag: ExtUtils-Git_1.143
    Checking prior to clean-up/perltidy

--- lib/ExtUtils/Git.pm
+++ lib/ExtUtils/Git.pm
@@ -52,7 +52,7 @@ use File::Grep qw(fdo);
 #  Version information in a formate suitable for CPAN etc. Must be
 #  all on one line
 #
-$VERSION = '1.142';
+$VERSION = '1.143';
 
 
 #  Load up our config file

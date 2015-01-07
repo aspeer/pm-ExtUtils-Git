6cc456700fee8ef6bf4894163c9ccf99a355602f
Andrew Speer <andrew.speer@isolutions.com.au>
2015-01-08 01:53:04 +1030

    VERSION increment: 1.171

--- lib/ExtUtils/Git.pm
+++ lib/ExtUtils/Git.pm
@@ -58,7 +58,7 @@ $ExtUtils::Manifest::Quiet=1;
 #  Version information in a format suitable for CPAN etc. Must be
 #  all on one line
 #
-$VERSION='1.170';
+$VERSION='1.171';
 
 
 #  All done, init finished
--- lib/ExtUtils/Git/MM.pm
+++ lib/ExtUtils/Git/MM.pm
@@ -37,7 +37,7 @@ use Cwd;
 #  Version information in a formate suitable for CPAN etc. Must be
 #  all on one line
 #
-$VERSION='1.170';
+$VERSION='1.171';
 
 
 #  use ExtUtils::MakeMaker as our parent class.

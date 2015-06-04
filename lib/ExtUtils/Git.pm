46803deb0ecfd17d530a6d366eaa485b15b10cf8
Andrew Speer <andrew.speer@isolutions.com.au>
2015-06-04 11:35:56 +0930

    VERSION increment: 1.174

--- lib/ExtUtils/Git.pm
+++ lib/ExtUtils/Git.pm
@@ -59,7 +59,7 @@ $ExtUtils::Manifest::Quiet=1;
 #  Version information in a format suitable for CPAN etc. Must be
 #  all on one line
 #
-$VERSION='1.173';
+$VERSION='1.174';
 
 
 #  All done, init finished

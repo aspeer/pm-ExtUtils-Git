a65d3e1c035cad3ff9ae4be6bfa7b1f0ef9c2d50
Andrew Speer <andrew.speer@isolutions.com.au>
2024-11-17 19:00:24 +1030

    Updated doc

--- lib/ExtUtils/Git.pm
+++ lib/ExtUtils/Git.pm
@@ -1,7 +1,7 @@
 #
 #  This file is part of ExtUtils::Git.
 #
-#  This software is copyright (c) 2022 by Andrew Speer <andrew.speer@isolutions.com.au>.
+#  This software is copyright (c) 2024 by Andrew Speer <andrew.speer@isolutions.com.au>.
 #
 #  This is free software; you can redistribute it and/or modify it under
 #  the same terms as the Perl 5 programming language system itself.
@@ -2697,7 +2697,7 @@ Andrew Speer <andrew.speer@isolutions.com.au>
 
 This file is part of ExtUtils::Git.
 
-This software is copyright (c) 2022 by Andrew Speer <andrew.speer@isolutions.com.au>.
+This software is copyright (c) 2024 by Andrew Speer <andrew.speer@isolutions.com.au>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
@@ -2705,6 +2705,7 @@ the same terms as the Perl 5 programming language system itself.
 Full license text is available at:
 
 <http://dev.perl.org/licenses/>
+
 
 =end markdown
 

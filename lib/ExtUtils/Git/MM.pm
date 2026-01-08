b627a2d27a0b2cb7f23ba95fa4911b79b977060e
Andrew Speer <andrew.speer@isolutions.com.au>
2026-01-08 22:10:46 +1030

    VERSION increment: 1.184

--- lib/ExtUtils/Git/MM.pm
+++ lib/ExtUtils/Git/MM.pm
@@ -38,7 +38,7 @@ use Cwd;
 #  Version information in a formate suitable for CPAN etc. Must be
 #  all on one line
 #
-$VERSION='1.183';
+$VERSION='1.184';
 
 
 #  use ExtUtils::MakeMaker as our parent class.
